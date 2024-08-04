4610ab1a9eeee4ad8c07c1cf11630099ede45be2
sayedalali1 <sayedalali1@gmail.com>
2024-08-04 12:48:38 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 // src/App.jsx
+import './App.css';
 
 const App = () => {
+  const books = [
+    { name: 'The Alchemist', read: true },
+    { name: 'Maktoob', read: false },
+    { name: 'The Secret', read: true },
+  ];
 
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Book List</h1>
+      <ul>
+        {books.map((book, index) => (
+          <li key={index} className={book.read ? 'completed' : 'not-completed'}>
+            {book.name}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
-export default App
+export default App;
